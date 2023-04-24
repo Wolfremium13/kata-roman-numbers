7b5d74085b5b405cd79c61bade554808b85f42d2
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-24 21:28:46 +0100

    ♻️ changed to functional

--- src/roman_numbers.py
+++ src/roman_numbers.py
@@ -1,8 +1,3 @@
 def roman_to_int(roman_number: str) -> int:
     roman_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result = 0
-    for letter in roman_number:
-        if letter not in roman_numbers:
-            return 0
-        result += roman_numbers[letter]
-    return result
+    return sum(roman_numbers.get(letter, 0) for letter in roman_number)
