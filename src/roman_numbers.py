108559e3c0925e45b3a8002074afb348e28f4d66
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-24 21:13:17 +0100

    ✅ parse all roman numbers

--- src/roman_numbers.py
+++ src/roman_numbers.py
@@ -1,2 +1,11 @@
 def roman_to_int(roman_number: str) -> int:
-    return 0 if roman_number == "" else 1
+    roman_numbers = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500, 
+        "M": 1000
+    }
+    return 0 if roman_number == "" else roman_numbers[roman_number]
--- tests/test_roman_numbers.py
+++ tests/test_roman_numbers.py
@@ -9,4 +9,10 @@ class TestRomanNumbersToInteger:
 
     def test_parse_roman_numbers(self):
         assert_that(roman_to_int("I")).is_equal_to(1)
+        assert_that(roman_to_int("V")).is_equal_to(5)
+        assert_that(roman_to_int("X")).is_equal_to(10)
+        assert_that(roman_to_int("L")).is_equal_to(50)
+        assert_that(roman_to_int("C")).is_equal_to(100)
+        assert_that(roman_to_int("D")).is_equal_to(500)
+        assert_that(roman_to_int("M")).is_equal_to(1000)
     
