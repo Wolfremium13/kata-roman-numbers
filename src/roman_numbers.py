c31b4780d2a13f45074b5adc7ae7d94d15711fed
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-24 21:11:06 +0100

    ✅ parse roman numbers

--- src/roman_numbers.py
+++ src/roman_numbers.py
@@ -1,2 +1,2 @@
 def roman_to_int(roman_number: str) -> int:
-    return 0
+    return 0 if roman_number == "" else 1
--- tests/test_roman_numbers.py
+++ tests/test_roman_numbers.py
@@ -6,3 +6,7 @@ from src.roman_numbers import roman_to_int
 class TestRomanNumbersToInteger:
     def test_ignore_empty_roman_number(self):
         assert_that(roman_to_int("")).is_equal_to(0)
+
+    def test_parse_roman_numbers(self):
+        assert_that(roman_to_int("I")).is_equal_to(1)
+    
