11b06b1235d6b327bc583cbbed563b132f586987
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 16:29:49 +0000

    🎉 initial commit

---
+++ kata-roman-numbers/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace kata_roman_numbers
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
