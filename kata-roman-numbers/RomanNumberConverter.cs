6f3f8a5245911530431748260712a20031d95856
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 08:46:24 +0000

    ✅ support_adding_until_three_repetitions

--- kata-roman-numbers/RomanNumberConverter.cs
+++ kata-roman-numbers/RomanNumberConverter.cs
@@ -4,44 +4,31 @@ namespace kata_roman_numbers
 {
     public class RomanNumberConverter
     {
+        private static readonly Dictionary<int, string> DecimalToRoman = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 500, "D" },
+            { 100, "C" },
+            { 50, "L" },
+            { 10, "X" },
+            { 5, "V" },
+            { 1, "I" }
+        };
+
         public static string Convert(int givenNumber)
         {
-            if (givenNumber == 0)
+            var romanNumber = string.Empty;
+            var enumerator = DecimalToRoman.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                return GetRomanNumber(givenNumber);
+                var roman = enumerator.Current.Value;
+                while (givenNumber >= enumerator.Current.Key)
+                {
+                    romanNumber += roman;
+                    givenNumber -= enumerator.Current.Key;
+                }
             }
-
-            if (givenNumber < 4)
-            {
-                return "I" + Convert(givenNumber - 1);
-            }
-
-            if (givenNumber > 5 && givenNumber < 9)
-            {
-                return "V" + Convert(givenNumber - 5);
-            }
-
-            if (givenNumber > 10 && givenNumber < 14)
-            {
-                return "X" + Convert(givenNumber - 10);
-            }
-
-            return GetRomanNumber(givenNumber);
-        }
-
-        private static string GetRomanNumber(int givenNumber)
-        {
-            var romanNumbersMap = new Dictionary<int, string>
-            {
-                { 1, "I" },
-                { 5, "V" },
-                { 10, "X" },
-                { 50, "L" },
-                { 100, "C" },
-                { 500, "D" },
-                { 1000, "M" }
-            };
-            return romanNumbersMap.TryGetValue(givenNumber, out var romanNumber) ? romanNumber : "";
+            return romanNumber;
         }
     }
 }
