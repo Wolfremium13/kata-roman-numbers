f3f69679376982d05d8422ce484dd5ce9b0454c2
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 08:53:34 +0000

    ✅ support_subtracting_until_three_repetitions

--- kata-roman-numbers/RomanNumberConverter.cs
+++ kata-roman-numbers/RomanNumberConverter.cs
@@ -7,12 +7,18 @@ namespace kata_roman_numbers
         private static readonly Dictionary<int, string> DecimalToRoman = new Dictionary<int, string>
         {
             { 1000, "M" },
+            { 900, "CM" },
             { 500, "D" },
+            { 400, "CD" },
             { 100, "C" },
+            { 90, "XC" },
             { 50, "L" },
+            { 40, "XL" },
             { 10, "X" },
+            { 9, "IX" },
             { 5, "V" },
-            { 1, "I" }
+            { 4, "IV" },
+            { 1, "I" },
         };
 
         public static string Convert(int givenNumber)
@@ -28,6 +34,7 @@ namespace kata_roman_numbers
                     givenNumber -= enumerator.Current.Key;
                 }
             }
+
             return romanNumber;
         }
     }
