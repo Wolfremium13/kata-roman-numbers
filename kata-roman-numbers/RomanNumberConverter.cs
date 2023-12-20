5a93f3dae0041e6106160fa6edd0e659d41cc57e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 07:14:27 +0000

    ❌ convert_decimal_to_roman

---
+++ kata-roman-numbers/RomanNumberConverter.cs
@@ -0,0 +1,10 @@
+namespace kata_roman_numbers
+{
+    public class RomanNumberConverter
+    {
+        public static string Convert(int i)
+        {
+            return "";
+        }
+    }
+}
---
+++ kata-roman-numbers/RomanNumberConverterShould.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace kata_roman_numbers
+{
+    [TestFixture]
+    public class RomanNumberConverterShould
+    {
+        [Test]
+        public void convert_decimal_to_roman()
+        {
+            Assert.That(RomanNumberConverter.Convert(1), Is.EqualTo("I"));
+        }
+    }
+}
