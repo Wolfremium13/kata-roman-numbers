a2d31491d612498170400b4de4d55bbc1603b133
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 07:39:02 +0000

    ♻ change to switch

--- kata-roman-numbers/RomanNumberConverter.cs
+++ kata-roman-numbers/RomanNumberConverter.cs
@@ -4,31 +4,23 @@ namespace kata_roman_numbers
     {
         public static string Convert(int givenNumber)
         {
-            if (givenNumber == 5)
+            switch (givenNumber)
             {
-                return "V";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+                default:
+                    return "I";
             }
-            if (givenNumber == 10)
-            {
-                return "X";
-            }
-            if (givenNumber == 50)
-            {
-                return "L";
-            }
-            if (givenNumber == 100)
-            {
-                return "C";
-            }
-            if (givenNumber == 500)
-            {
-                return "D";
-            }
-            if (givenNumber == 1000)
-            {
-                return "M";
-            }
-            return "I";
         }
     }
 }
