ae32fa5d15a13fe20b0de3f304fcb85a0c491ece
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 07:16:57 +0000

    ♻ rename parameter

--- kata-roman-numbers/RomanNumberConverter.cs
+++ kata-roman-numbers/RomanNumberConverter.cs
@@ -2,7 +2,7 @@ namespace kata_roman_numbers
 {
     public class RomanNumberConverter
     {
-        public static string Convert(int i)
+        public static string Convert(int givenNumber)
         {
             return "I";
         }
