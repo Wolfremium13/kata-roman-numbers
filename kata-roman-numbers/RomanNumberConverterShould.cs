f1de1364a3272d161ce93eef3d73d557506d94e0
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-20 07:32:33 +0000

    ❌ convert_decimal_to_roman

--- kata-roman-numbers/RomanNumberConverterShould.cs
+++ kata-roman-numbers/RomanNumberConverterShould.cs
@@ -9,12 +9,12 @@ namespace kata_roman_numbers
         public void convert_decimal_to_roman()
         {
             Assert.That(RomanNumberConverter.Convert(1), Is.EqualTo("I"));
-            Assert.That(RomanNumberConverter.Convert(2), Is.EqualTo("II"));
             Assert.That(RomanNumberConverter.Convert(5), Is.EqualTo("V"));
             Assert.That(RomanNumberConverter.Convert(10), Is.EqualTo("X"));
             Assert.That(RomanNumberConverter.Convert(50), Is.EqualTo("L"));
             Assert.That(RomanNumberConverter.Convert(100), Is.EqualTo("C"));
             Assert.That(RomanNumberConverter.Convert(500), Is.EqualTo("D"));
+            Assert.That(RomanNumberConverter.Convert(1000), Is.EqualTo("M"));
         }
     }
 }
